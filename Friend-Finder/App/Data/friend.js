7070918d90d3447e8ee8464b6b0bbafd08cdaec0
FlipClown <sourmento@gmail.com>
2017-11-11 16:43:59 -0500

    Add files via upload

---
+++ Friend-Finder/App/Data/friend.js
@@ -0,0 +1,43 @@
+var friends = [{
+    "name": "Son Goku",
+    "photo": "http://media.comicbook.com/2017/08/goku-1014997-1280x0.jpg",
+    "scores": [5,2,2,5,5,1,3,2,5,2]
+}, {
+    "name": "Vegeta",
+    "photo": "http://media.comicbook.com/2017/03/vegeta-dragon-ball-super-237496-1280x0.png",
+    "scores": [1,5,3,4,4,5,2,1,4,3]
+}, {
+    "name": "Son Gohan",
+    "photo": "https://i.ytimg.com/vi/xZ1S8YGAKCk/maxresdefault.jpg",
+    "scores": [5,3,3,4,5,5,5,3,5,2]
+}, {
+    "name": "Piccolo",
+    "photo": "https://lh3.googleusercontent.com/-q9DNxDRrdpE/VmVhFB5mzdI/AAAAAAAAAHw/2lC1yy0-DIE/w1099-h813/image.jpeg",
+    "scores": [1,1,1,4,5,5,4,3,5,3]
+}, {
+    "name": "Krillin",
+    "photo": "http://pm1.narvii.com/6446/676fbbb670cc1b557b6907f85d8a1b97a9c3f551_hq.jpg",
+    "scores": [3,2,5,1,1,1,4,5,5,5]
+}, {
+    "name": "Tienshinhan",
+    "photo": "https://i.ytimg.com/vi/7Mk1id0Vp8s/maxresdefault.jpg",
+    "scores": [1,4,5,3,2,4,4,5,1,2]
+}, {
+    "name": "Frieza",
+    "photo": "http://nerdreactor.com/wp-content/uploads/2017/05/freeza-happy2.jpg",
+    "scores": [3,4,5,1,2,3,4,5,2,1]
+}, {
+    "name": "Hercule",
+    "photo": "http://i.imgur.com/fIQwXUI.jpg",
+    "scores": [3,2,2,2,1,1,5,1,3,4]
+}, {
+    "name": "Beerus",
+    "photo": "https://pre00.deviantart.net/9914/th/pre/f/2017/054/b/f/dragon_ball_super_beerus_10_by_giuseppedirosso-db05jca.jpg",
+    "scores": [5,2,5,1,1,4,3,5,5,1]
+}, {
+    "name": "Whis",
+    "photo": "http://2.bp.blogspot.com/-ImdqIAnKgM0/Vf696EWfEHI/AAAAAAAACLM/8N3RgZf_NYw/s1600/Let%2527s%2BGo%2BGoku%2B7.png",
+    "scores": [2,4,5,3,1,4,5,2,1,3]
+}];
+
+module.exports = friends;
---
+++ Friend-Finder/App/Routing/apiRoutes.js
@@ -0,0 +1,43 @@
+// dependencies
+var path = require('path');
+
+
+var friend = require('../data/friend.js');
+
+// Export API routes
+module.exports = function (app) {
+
+    app.get('/api/friend', function (req, res) {
+        res.json(friend);
+    });
+
+    // Add friend 
+    app.post('/api/friend', function (req, res) {
+
+        var userInput = req.body;
+        var userResponses = userInput.scores;
+        var matchedName = '';
+        var matchedPhoto = '';
+        var checkNum = 10000;
+
+
+        for (var i = 0; i < friend.length; i++) {
+
+            var diff = 0;
+            for (var j = 0; j < userResponses.length; j++) {
+                diff += Math.abs(friend[i].scores[j] - userResponses[j]);
+            }
+
+            if (diff < checkNum) {
+                checkNum = diff;
+                matchedName = friend[i].name;
+                matchedPhoto = friend[i].photo;
+            }
+        }
+
+
+        friend.push(userInput);
+
+        res.json({ status: 'OK', matchedName: matchedName, matchedPhoto: matchedPhoto });
+    });
+};
